c8d5c3df5efebe7b3aed4ea37c462e978df8aecc
BrunoMeloeSilva <brunomeloesilva@gmail.com>
2017-08-28 14:51:41 -0400

    Generalizacao do codigo para N colunas e escolha do separador

--- src/bcms/util/MountText.java
+++ src/bcms/util/MountText.java
@@ -12,14 +12,14 @@ public class MountText {
 		// Descobrir a quantidade de colunas de "Variaveis.txt"
 		// Inicio
 		String line = null;
-		int posSemicolon = 0;
+		int positionSeparator = 0;
 		int numberOfColumns = 0;
 		if ((line = tuple.readLine()) != null) {
-			while ((posSemicolon = line.indexOf(variableTextFileSeparator)) != -1) {
+			while ((positionSeparator = line.indexOf(variableTextFileSeparator)) != -1) {
 				numberOfColumns++;
-				line = line.substring(++posSemicolon, line.length());
+				line = line.substring(++positionSeparator, line.length());
 			}
-			posSemicolon = 0;
+			positionSeparator = 0;
 		}
 		System.out.println("--> " + numberOfColumns+" colunas identificadas, proceso de escrita iniciado..\n");
 		String[] valores = new String[numberOfColumns];
@@ -28,18 +28,18 @@ public class MountText {
 			// obtem valores da linha atual do arquivo de variaveis
 			// Inicio
 			for (int i = 0; i < numberOfColumns; i++) {
-				line = line.substring(posSemicolon, line.length());
-				posSemicolon = line.indexOf(variableTextFileSeparator) + 1;
+				line = line.substring(positionSeparator, line.length());
+				positionSeparator = line.indexOf(variableTextFileSeparator) + 1;
 				valores[i] = line.substring(0, line.indexOf(variableTextFileSeparator));
 			}
-			posSemicolon = 0;
+			positionSeparator = 0;
 			// Fim
 			// replace no arquivo de comando, o arquivo inteiro é considerado
 			// como uma linha
 			BufferedReader fileCmdTxt = new BufferedReader(new FileReader(commandTextFilePath));
 			while ((line = fileCmdTxt.readLine()) != null) {
 				for (int j = 0; j < numberOfColumns; j++) {
-					line = line.replace("$" + j, valores[j]);
+					line = line.replace("$" + j +"$", valores[j]);
 				}
 				System.out.println(line);
 			}
@@ -49,13 +49,15 @@ public class MountText {
 	}
 
 	public static void main(String[] args) throws IOException {
-		mountText("Variaveis.txt", '	', "Comandos.txt");
+		mountText("Variaveis.txt", ';', "Comandos.txt");
 	}
 }
 /*
- * Limitações: a. O arquivo de variaveis só pode N colunas, exemplo: a;b;...N;
+ * a. O arquivo de variaveis pode N colunas, exemplo: a;b;...N;
  * b. A primeira linha do aquivo de variaveis deve ser o cabecalho, mas será
  * desconsiderado na saída. Obs.: Se estiver executando no eclipse, deve-se
  * aumentar o limite de caracteres de saida em (No console, botão direito,
  * Preferences, Limit console output)
+ * c. O arquivo de comandos deve seguir a seguinte regra para os valores que serão substituidos pela respectiva
+ * posição no arquivo de variaveis, $posicao$ , exemplo: $0$ para pegar o valor na posição 0 do arquivo de variaveis.
  */
